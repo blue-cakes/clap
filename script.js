c0497a0b9e4bc4c7d7096f0f5e664d3d8bb5dcb2
blue cakes <77317164+blue-cakes@users.noreply.github.com>
2021-06-09 22:03:47 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">{{ count }} 👏 claps</button>'
+})
+
+let app = new Vue ({
+  el: '#app',
+  data: {
+    myData: 'Super cool VueJS application 🖖'
+  }
+})
